41933c2d9c08951f7bd0733262db6fb2f698aad4
Alina Ivashchenko <98417787+Alina-Ivashchenko@users.noreply.github.com>
2025-02-05 10:29:42 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Ensure relative paths are used
-  build: {
-    outDir: 'dist', // Vercel expects this directory for static assets
-  }
 })
