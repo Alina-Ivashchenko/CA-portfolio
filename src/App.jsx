abd47ab8a60f2a65e420b04e06b96d1ba48f672f
Alina Ivashchenko <98417787+Alina-Ivashchenko@users.noreply.github.com>
2025-02-05 12:42:25 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home page */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
