0b5ea12f88d5e342f1c18afc5570322d1e13178d
Alina Ivashchenko <98417787+Alina-Ivashchenko@users.noreply.github.com>
2025-03-31 11:39:35 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer>
-        <p>&copy; {new Date().getFullYear()} Alina Ivashchenko | Community Advisor Candidate</p>
+        <p>&copy; {new Date().getFullYear()} Alina Ivashchenko | International Student Leader</p>
 
       </footer>
     );
